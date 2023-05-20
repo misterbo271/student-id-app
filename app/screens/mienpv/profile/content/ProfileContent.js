32f133e514d6998043da56dc31c3f2b74a8442de
MienPV <mien.pham@vexere.com>
2023-05-20 12:45:20 +0700

    fix bug

--- app/screens/mienpv/profile/content/ProfileContent.js
+++ app/screens/mienpv/profile/content/ProfileContent.js
@@ -8,6 +8,7 @@ import {appStyles} from 'configs/styles';
 import {strings} from 'controls/i18n';
 import dimens from 'configs/dimens';
 import {observer} from 'mobx-react';
+import CBGlobal from "globals/CBGlobal";
 
 const BodyBlock = observer(({style, contentStyle, code, type, name, color, size, onPress}) => {
 
@@ -28,6 +29,7 @@ const ProfileContent = ({defaultParam, onPress, onLogout, onPressUrl, onEditProf
     const {theme} = useTheme();
     const cbActionSheetRef = useRef(null);
     const [info, setInfo] = useState(null);
+    const {_id, address, name, phone_number, status, email, day_of_birth, id_back_image, id_front_image, selfie_image} = CBGlobal.userInfo;
 
     useEffect(() => {
         loadApplicationOrProfileEvent();
@@ -51,10 +53,13 @@ const ProfileContent = ({defaultParam, onPress, onLogout, onPressUrl, onEditProf
             <CBView style={[appStyles.row, {paddingVertical: 15, paddingHorizontal: 30, marginTop: dimens.statusBar}]} define={'none'}>
                 <CBTouchableOpacity style={{flex: 1, marginRight: 15}} define={'none'} onPress={onEditProfile}>
                     <CBView style={appStyles.row} define={'none'}>
-                        <CBText style={[appStyles.title, {color: '#FFFFFF'}]} allowFontScaling={false} define={'none'}>{'Mien PV'}</CBText>
+                        <CBText style={[appStyles.title, {color: '#FFFFFF'}]} allowFontScaling={false} define={'none'}>{name}</CBText>
+                        {status === 'verified' ? <CBIcon containerStyle={{marginLeft: 5}} type={'material'} name={'verified'} color={'#FFFFFF'} size={20}/> : null}
+                    </CBView>
+                    <CBView style={appStyles.row}>
+                        <CBText style={[appStyles.text, {marginRight: 5, color: '#FFFFFF'}]} allowFontScaling={false} define={'none'}>{_id}</CBText>
                         <CBIcon containerStyle={{marginLeft: 5}} type={'ionicon'} name={'create-outline'} color={'#FFFFFF'} size={20}/>
                     </CBView>
-                    <CBText style={[appStyles.text, {color: '#FFFFFF'}]} allowFontScaling={false} define={'none'}>{'123456789'}</CBText>
                 </CBTouchableOpacity>
                 <CBAvatar
                     containerStyle={{borderRadius: 15}}
@@ -69,12 +74,22 @@ const ProfileContent = ({defaultParam, onPress, onLogout, onPressUrl, onEditProf
                 showsVerticalScrollIndicator={false}
                 refreshControl={<CBRefreshControl/>}
                 define={'content'}>
-                <BodyBlock style={{marginBottom: 15}} code={'Học phí'} type={'ionicon'} name={'information-circle-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>
-                <BodyBlock style={{marginBottom: 15}} code={'E-learning'} type={'ionicon'} name={'analytics-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>
-                <BodyBlock style={{marginBottom: 15}} code={'Thời khoá biểu'} type={'ionicon'} name={'business-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>
-                <BodyBlock style={{marginBottom: 15}} code={'Lịch thi'} type={'ionicon'} name={'book-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>
-                <BodyBlock style={{marginBottom: 15}} code={'Bảng điểm'} type={'ionicon'} name={'bus-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>
-                <BodyBlock style={{marginBottom: 15}} code={'Đăng ký môn học'} type={'ionicon'} name={'bus-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>
+                <CBView style={[{marginBottom: 15}, {borderRadius: 15, backgroundColor: theme.colors.hide}]} define={'none'}>
+                    <CBTouchableOpacity style={[appStyles.item, {borderRadius: 15}, appStyles.divider, {borderTopWidth: 1}]} define={'divider'} onPress={onLogout}>
+                        <CBIcon type={'ionicon'} name={'call-outline'} color={theme.colors.primary} size={25}/>
+                        <CBText style={[appStyles.text, {flex: 1, marginLeft: 15, marginRight: 15}]} define={'text'}>{phone_number}</CBText>
+                    </CBTouchableOpacity>
+                    <CBTouchableOpacity style={[appStyles.item, {borderRadius: 15}]} define={'none'} onPress={onPress({refId: 'Setting'})}>
+                        <CBIcon type={'material-community'} name={'email-outline'} color={theme.colors.primary} size={25}/>
+                        <CBText style={[appStyles.text, {flex: 1, marginLeft: 15, marginRight: 15}]} define={'text'}>{email}</CBText>
+                    </CBTouchableOpacity>
+                </CBView>
+                {/*<BodyBlock style={{marginBottom: 15}} code={'Học phí'} type={'ionicon'} name={'information-circle-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>*/}
+                {/*<BodyBlock style={{marginBottom: 15}} code={'E-learning'} type={'ionicon'} name={'analytics-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>*/}
+                {/*<BodyBlock style={{marginBottom: 15}} code={'Thời khoá biểu'} type={'ionicon'} name={'business-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>*/}
+                {/*<BodyBlock style={{marginBottom: 15}} code={'Lịch thi'} type={'ionicon'} name={'book-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>*/}
+                {/*<BodyBlock style={{marginBottom: 15}} code={'Bảng điểm'} type={'ionicon'} name={'bus-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>*/}
+                {/*<BodyBlock style={{marginBottom: 15}} code={'Đăng ký môn học'} type={'ionicon'} name={'bus-outline'} color={theme.colors.primary} size={25} onPress={onPress}/>*/}
                 <CBView style={[{marginBottom: 15}, {borderRadius: 15, backgroundColor: theme.colors.hide}]} define={'none'}>
                     <CBTouchableOpacity style={[appStyles.item, {borderRadius: 15}]} define={'none'} onPress={onPress({refId: 'Setting'})}>
                         <CBIcon type={'ionicon'} name={'settings-outline'} color={theme.colors.primary} size={25}/>
